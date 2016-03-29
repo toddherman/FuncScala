344e4b566ca3ae68cd6bfeb9aa75c4f64753e666
toddherman <todd@hermanconsult.com>
2016-03-29 16:39:26 -0700

    Proof of OO purity.

--- src/week4/Nat.scala
+++ src/week4/Nat.scala
@@ -1,9 +1,14 @@
 package week4
 
+//Is Scala a pure OO language?
+//Yes, we can implement all primitive types and operators (i.e. boolean, int) 
+//via classes (Boolean, Int, etc...).
+
+//Peano numbers
 abstract class Nat {
   def isZero: Boolean
   def predecessor: Nat
-  def successor: Nat
+  def successor = new Succ(this) // refactored
   def +(that: Nat): Nat
   def -(that: Nat): Nat
 }
@@ -11,13 +16,13 @@ abstract class Nat {
 object Zero extends Nat {
   def isZero = true
   def predecessor = throw new Error("0.predecessor")
-  def successor = new Succ(this)
-  def +(that: Nat)
-  dev - (that: Nat)
+  def +(that: Nat) = that // zero plus a number is the number
+  def -(that: Nat) = if (that.isZero) this else throw new Error("negative number")
 }
 
-class Succ(N: Nat) extends Nat {
+class Succ(n: Nat) extends Nat {
   def isZero = false
   def predecessor = n
-  def successor = new Succ(this) // refactor this as it is duplicated in Zero above
+  def +(that: Nat) = new Succ(n + that) // recursive
+  def -(that: Nat) = if (that.isZero) this else n - that.predecessor
 }
