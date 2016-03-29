9f127ee826f69ecb608e6d6508a8b2a7fab3ced4
toddherman <todd@hermanconsult.com>
2016-03-28 17:26:04 -0700

    Exploring functions as objects.

--- src/week3/List.scala
+++ src/week3/List.scala
@@ -22,3 +22,17 @@ class Nil[T] extends List[T] {
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
   // because Nothing is a subtype of any other type.
 }
+
+//  Looking at functions as objects
+//  define an object List {
+//  with 3 functions in it so that users can create lists of lengths 0-2
+object List {
+  // List(1,2) would expand to List.apply(1,2)
+
+  // parameterize apply with T
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+
+  // for empty list
+  def apply[T]() = new Nil
+
+}
