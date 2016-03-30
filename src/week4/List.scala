9fa37a9bc5678ae91916b917ac78e6427e9ff54f
toddherman <todd@hermanconsult.com>
2016-03-29 17:38:58 -0700

    Nil modeled as an object. Added covariant type parameter.

--- src/week4/List.scala
+++ src/week4/List.scala
@@ -1,6 +1,7 @@
 package week4
 
-trait List[T] {
+// added covariant type parameter to T here.
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
@@ -10,11 +11,13 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false // because they are never empty
 }
 
-//One shortcoming there that was that nil was modeled as a class. 
-//Whereas we would prefer it to be an object, after all there's only one empty list.
-// Can we change that?  Yes, because we can make List covariant.
-class Nil[T] extends List[T] {
+//Nil modeled as an object. 
+// Because 'Nothing' is the bottom type, which is a subtype of every other type
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+object test {
+  val x: List[String] = Nil // should be the empty list.
 }
